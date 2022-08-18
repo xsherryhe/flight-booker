896fb0438647c3d53f069d403400afe748916363
Sherry He <xsherryhe@gmail.com>
2022-08-18 15:02:21 -0400

    Created Airport model and seeded database

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+Airport.delete_all
+[['San Francisco International', 'SFO'], 
+ ['John F. Kennedy International', 'JFK'],
+ ["Chicago O'Hare International", 'ORD'],
+ ['Denver International', 'DEN'],
+ ['Hong Kong International', 'HKG'],
+ ['Tokyo International', 'HND'],
+ ['Paris Charles de Gaulle', 'CDG']].each do |name, code|
+    Airport.create(name: name, code: code)
+ end
