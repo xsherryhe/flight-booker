8a9ee644abe941fe13473a22c44737f47ccaff12
Sherry He <xsherryhe@gmail.com>
2022-08-19 19:20:00 -0400

    Added CSS styling to flights#index

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -2,7 +2,7 @@ class FlightsController < ApplicationController
   def index
     @start_year, @end_year = Flight.year_range
     @airport_options = Airport.all
-    @page = params[:page].to_i || 1
+    @page = (params[:page] || 1).to_i
     @search_params = params[:search] ? Flight.parse_search_params(search_params) : {}
     @all_flights = Flight.matching_search(@search_params) if params[:search]
     @page_flights = @all_flights.from_page(@page, 10) if @all_flights
