78eaf245d6f1d751d84e66e4a950f01300f6f3a4
Sherry He <xsherryhe@gmail.com>
2022-08-27 19:10:25 -0400

    Added confirmation emails feature to bookings

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -8,6 +8,7 @@ class BookingsController < ApplicationController
     @booking = Booking.new(booking_params)
 
     if @booking.save
+      send_confirmation_emails
       redirect_to @booking
     else
       @booking.passengers.build while @booking.passengers.size < params[:number_of_passengers].to_i
@@ -27,5 +28,12 @@ class BookingsController < ApplicationController
           .permit(:flight_id,
                   passengers_attributes:
                     %i[first_name middle_name last_name date_of_birth email])
+  end
+
+  def send_confirmation_emails
+    @booking.passengers.each do |passenger|
+      PassengerMailer.with(passenger:, booking: @booking)
+                     .confirmation_email.deliver_later
+    end
   end
 end
--- app/mailers/passenger_mailer.rb
+++ app/mailers/passenger_mailer.rb
@@ -1,2 +1,9 @@
 class PassengerMailer < ApplicationMailer
+  def confirmation_email
+    @passenger = params[:passenger]
+    @booking = params[:booking]
+    @flight = @booking.flight
+    date = @flight.departure_time.strftime('%-m/%-d/%Y')
+    mail(to: @passenger.email, subject: "Your flight on #{date} is booked!")
+  end
 end
