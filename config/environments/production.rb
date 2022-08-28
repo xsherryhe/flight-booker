f4c3d3c6dfcf1a035dd91f21c94f94447eb754c1
Sherry He <xsherryhe@gmail.com>
2022-08-28 18:07:35 -0400

    Fixed additional bug in smtp settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,14 +91,13 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.perform_deliveries = true
   config.action_mailer.default_url_options = { host: 'rocky-shore-24771.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
     port: ENV['MAILGUN_SMTP_PORT'],
     address: ENV['MAILGUN_SMTP_SERVER'],
-    username: ENV['MAILGUN_SMTP_LOGIN'],
+    user_name: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
     domain: 'rocky-shore-24771.herokuapp.com',
     authentication: :plain
