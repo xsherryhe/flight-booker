35c3d330d3affc1391302f0fdefde6f44e86c6a2
Sherry He <xsherryhe@gmail.com>
2022-08-28 17:35:08 -0400

    Minor edits to production email settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,7 +92,7 @@ Rails.application.configure do
   config.active_record.dump_schema_after_migration = false
 
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.default_url_options = { host: 'rocky-shore-24771.herokuapp.com'  }
+  config.action_mailer.default_url_options = { host: 'rocky-shore-24771.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   config.action_mailer.smtp_settings = {
@@ -101,6 +101,7 @@ Rails.application.configure do
     username: ENV['MAILGUN_SMTP_LOGIN'],
     password: ENV['MAILGUN_SMTP_PASSWORD'],
     domain: 'rocky-shore-24771.herokuapp.com',
-    authentication: :plain
+    authentication: :plain,
+    enable_starttls_auto: true
   }
 end
